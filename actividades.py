d1766ca7d6029eead08768fc412e65fd3c38b8d5
Kevin Luciano <78965771+kalg12@users.noreply.github.com>
2025-03-03 11:47:00 -0600

    Actividades del segundo parcial

---
+++ actividades.py
@@ -0,0 +1,41 @@
+#Número se asigna un valor a una variable
+numero = 10
+
+#Se imprime el valor de la variable número, ej. 10
+print("El número asignado a la variable número es: ", numero)
+
+"""
+Actividad de clase: Van a definir valores de variables y las van a imprimir; sobre algún hobby
+Usarás:
+- Enteros
+- Booleanos
+- Caracteres
+Tiempo: 8 min
+"""
+
+""" SENTENCIA IF - ES UNA CONDICIÓN SENTENCIA """
+if numero < 100:
+    print("La variable número es menor que el 100")
+
+"""
+Actividad definir 3 tipos de if
+- Comparación de un número contra otro
+- Ingresar a un lugar con la edad mínima
+- Sacar la INE
+"""
+
+""" SENTENCIA IF - ELSE """
+if numero <= 20:
+    print("El numéro es menor o igual a 20")
+else:
+    print("El número es mayor que 20")
+
+
+"""
+Actividad: Crear 1 IF-ELSE al azar
+"""
+
+""" Investiguen un juego en Python a base de IF-ELSE
+Copialo y pégalo aquí
+Comenta cada línea para que interpretes su funcionamiento
+"""
